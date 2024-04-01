91fee7f68204db4c79e9e23ddb60dcd097b7edf1
Carine Arantes <122240670+CarineArantes@users.noreply.github.com>
2024-04-01 20:31:46 -0300

    new

--- 25-03-2024/Soma/main.dart
+++ 25-03-2024/Soma/main.dart
@@ -7,7 +7,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
@@ -33,29 +33,44 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _y = 0;
   int _x = 0;
+  int _random = 0;
   final resp = TextEditingController();
   Icon certo = const Icon(Icons.check);
   Icon errado = const Icon(Icons.close);
   Icon saida = const Icon(Icons.question_mark);
-  int cont = 0;
-  Map _map_resp = {};
+  Map<int, dynamic> _map_resp = {};
 
+  @override
   void initState() {
     super.initState();
-    _y = Random().nextInt(10);
     _x = Random().nextInt(10);
   }
 
   void resultado() {
     setState(() {
       if ((_x + _y) == int.tryParse(resp.text)) {
-        _map_resp.addAll({cont: certo});
-      } else {
-        _map_resp.addAll({cont: errado});
+        _map_resp[_y] = _x + _y;
+        if (_map_resp.length == 10) {
+          _x = Random().nextInt(10);
+          _map_resp.clear();
+        } else {
+          int novoNumero;
+          do {
+            novoNumero = Random().nextInt(10) + _x;
+          } while (_map_resp.containsValue(novoNumero));
+          _y = novoNumero - _x;
+        }
       }
-      _y = Random().nextInt(10);
+      resp.clear();
+    });
+  }
+
+  void novo() {
+    setState(() {
       _x = Random().nextInt(10);
-      cont++;
+      _y = Random().nextInt(10);
+      _map_resp.clear();
+      resp.clear();
     });
   }
 
@@ -66,40 +81,58 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-                margin: const EdgeInsets.all(15),
-                width: 200,
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    labelText: '$_x + $_y = ',
+      body: Column(
+        children: [
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(15),
+                  width: 200,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      labelText: '$_x + $_y = ',
+                    ),
+                    onChanged: (value) {
+                      resp.text = value;
+                    },
+                    controller: resp,
                   ),
-                  onChanged: (value) {
-                    resp.text = value;
-                  },
-                  controller: resp,
-                )),
-
-            // ========> atualizar dinamicamente <===========
-            // Table(
-            //   border: TableBorder.all(),
-            //   children: [
-            //     _map_resp.forEach((key, value) {
-            //       // const TableRow(children: [Text("Oii")]);
-            //     })
-            //   ],
-            // ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => resultado(),
-        tooltip: 'Soma',
-        child: const Icon(Icons.add),
+                ),
+                FloatingActionButton(
+                  onPressed: () => resultado(),
+                  tooltip: 'Soma',
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
+          ),
+          DataTable(
+            columns: List.generate(
+              10,
+              (index) => DataColumn(label: Text('$_x + $index')),
+            ),
+            rows: List.generate(
+              1,
+              (index) => DataRow(
+                cells: List.generate(
+                  10,
+                  (index) => DataCell(
+                    // Alimentar a célula correta com base no mapa de resposta
+                    Text('${_map_resp[index] ?? 'x'}'),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          FloatingActionButton(
+                  onPressed: () => novo(),
+                  tooltip: 'Novo',
+                  child: const Icon(Icons.refresh),
+                ),
+        ],
       ),
     );
   }
